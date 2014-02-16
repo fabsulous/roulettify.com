cebc00ff9e6faf80258483ec90669fb0ab55e9de
Fabian Pröbstl <f.proebstl@gmx.net>
2014-02-16 01:58:09 +0100

    Made it work on azure

--- web/server.js
+++ web/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/lunch/', function(req, res) {
-    res.send('Hello World!')
+   res.send('Hello World!');
 });
 
-app.listen(80);
-console.log('Listening on port 80');
+app.listen(port);
